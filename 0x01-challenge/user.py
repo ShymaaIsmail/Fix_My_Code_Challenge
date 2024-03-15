46ce84b42123f49288ef376d7114a304420c5ca3
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-15 07:23:22 +0300

    tak2

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -7,11 +7,14 @@ User class
 class User():
     """ Documentation """
 
-    __email = None
-
     def __init__(self):
         """ Documentation """
         self.__email = None
+
+    @property
+    def email(self):
+        """ Documentation """
+        return self.__email
 
     @email.setter
     def email(self, value):
@@ -19,11 +22,6 @@ class User():
         if type(value) is not str:
             raise TypeError("email must be a string")
         self.__email = value
-
-    @property
-    def email(self):
-        """ Documentation """
-        return self.__email
 
 
 if __name__ == "__main__":
