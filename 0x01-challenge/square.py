b35aac3f866c62ef8dda7fa0d57bf7238ab83538
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-15 06:00:54 +0300

    jj

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,35 +1,33 @@
 #!/usr/bin/python3
-""" Square Class"""
+""" Square module. """
 
 
 class Square():
-    """ class square docs"""
+    """ Square class. """
+    width = 0
+    height = 0
 
     def __init__(self, *args, **kwargs):
-        """ init function for Square"""
-        self.width = 0
-        self.height = 0
+        """ Square class initialized """
         for key, value in kwargs.items():
             setattr(self, key, value)
-        if self.height != self.width:
-            raise ValueError("Square height must be equal to square width")
 
     def area_of_my_square(self):
-        """ Area of the Square """
+        """ Area of the square """
         return self.width * self.height
 
-    def perimeter_of_my_square(self):
-        """Permiter of the Square """
+    def permiter_of_my_square(self):
+        """ Permiter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """stringify the Square class"""
+        """ String representation of the square instance """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    """ test sample of square"""
+    """ Create Square instance """
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.perimeter_of_my_square())
+    print(s.permiter_of_my_square())
