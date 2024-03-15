88a26f1dedc71b9a96c0a4761654ef537b51bd71
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-15 05:26:23 +0300

    LIK

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -5,6 +5,9 @@
 class Square():
     """ class square docs"""
 
+    width = 0
+    height = 0
+
     def __init__(self, *args, **kwargs):
         """ init function for Square"""
         for key, value in kwargs.items():
@@ -25,7 +28,7 @@ class Square():
 
 if __name__ == "__main__":
 
-    s = Square(width=12, height=12)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_Square())
     print(s.perimeter_of_my_square())
