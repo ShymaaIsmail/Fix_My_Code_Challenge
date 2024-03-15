7efdcbe63b91b36b38985752e503ce9aa6727e9c
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-15 05:30:49 +0300

    self.width = 0         self.height = 0

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -5,11 +5,10 @@
 class Square():
     """ class square docs"""
 
-    width = 0
-    height = 0
-
     def __init__(self, *args, **kwargs):
         """ init function for Square"""
+        self.width = 0
+        self.height = 0
         for key, value in kwargs.items():
             setattr(self, key, value)
 
