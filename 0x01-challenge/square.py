991b8a0855e0705dfa921777af92fb8f16a1dbc2
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-15 05:12:12 +0300

    fix sqauare

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+""" Square Class"""
 
 class square():
-    
+
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """ init function for square"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,10 +16,13 @@ class square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
+        """Permiter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """stringify the square class"""
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
